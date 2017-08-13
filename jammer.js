d1f3ed7412e43cc2dfedce7b36e778860361ccd7
Salih Gedik <dev@salihgedik.com>
2017-08-13 19:57:29 +0300

    Strict mode problem fix

--- jammer.js
+++ jammer.js
@@ -1,21 +1,24 @@
 /**
  * Created by sg on 7/9/17.
  */
-
+'use strict';
 function $(element) {
-    if (element == document || element == window || element == history || element == location ) {
-        this.element = element;
-    } else {
-        this.element = document.querySelector(element);
-    }
-    return new jammerElement(this.element);
+    return  (element == document ||
+            element == window ||
+            element == history ||
+            element == location) ? element :
+             new jammerElement(document.querySelector(element));
+
 }
 
 var jammerElement = (function () {
     function jammer(element) {
+        //this.e = this;
         this.element = element;
         this.text = function (text) {
             this.element.textContent = text;
+            //console.log(this);
+            // console.log(this == e)
             return this;
         };
         this.on = function (event, callback) {
